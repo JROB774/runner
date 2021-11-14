2a753eaac99a6bae67e170c8d16d091b34d0ea52
Joshua Robertson <joshrobertson774@gmail.com>
2021-11-14 15:13:51 +0000

    Added custom HTML for packaging the web release of the game.

--- build/web/build.bat
+++ build/web/build.bat
@@ -7,6 +7,6 @@ if not exist %OutputPath% mkdir %OutputPath%
 
 call timer.bat emcc %InputSource% %IncludeDirs% %Libraries% %Defines% %CompilerFlags% %LinkerFlags% --preload-file %AssetPath% -o %OutputExecutable%.html
 
-:: copy dev\web\index.html %OutputPath%\index.html
+copy source\Platform\HTML\index.html %OutputPath%\index.html
 
 endlocal
